0ed6892fe7a038b1783cdd3bff25221536f21a5f
Tim Mitra <tim@it-guy.com>
2023-12-15 20:10:16 -0500

    Initial Commit

---
+++ TimeForCube/TimeForCubeApp.swift
@@ -0,0 +1,21 @@
+//
+//  TimeForCubeApp.swift
+//  TimeForCube
+//
+//  Created by Tim Mitra on 2023-12-15.
+//
+
+import SwiftUI
+
+@main
+struct TimeForCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
